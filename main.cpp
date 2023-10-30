77fad25687c04a2736c61363763d10f82799b35e
Sean Higgins <seanhiggins639@gmail.com>
2023-10-30 13:07:04 -0700

    Beginning OpenMP Implementation
    Beginning to implement the parallelization of or code using a CPU. Looking into GPU parallelization.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <stdio.h>	// printf and fprintf live here.
 #include <stdlib.h> // For dynamic memory allocation
+#include <cstring>  // For memset
+#include <omp.h>	// OpenMP, for multicore parallelization.
 
 #include "cluster_dynamics.hpp"
+
+//#define CSV		// Useful if we want to change the output format from text
+					// to pure CSV.
+
 
 #ifndef CONCENTRATION_BOUNDS
 #define CONCENTRATION_BOUNDS 100 
 #endif
+
+#ifndef NUMT
+#define	NUMT	2
+#endif;
 
 // result arrays of interstitials and vacancies
 
@@ -16,6 +27,8 @@
 
 int concentration_bounds;   // Setting an overall global variable to hold the concentration_bounds
                             // so that we aren't forces to pass the variable to every single function. - Sean H.
+
+int num_threads;			// Set the default number of OpenMP threads to use in the for-loop section.
 
 // number of clusters of N interstitials (in) per unit volume
 /* Pokor et al. 2004, 2a
@@ -121,42 +134,56 @@ double v_clusters(int vn, double* vacans, NuclearReactor& reactor, Material& mat
 
 int main(int argc, char* argv[])
 {
-    // Pass the value of the CONCENTRATION_BOUNDS via the command-line.
+    // Pass the values of the CONCENTRATION_BOUNDS and NUMT via the command-line.
     // If no value was passed, we can simply rely on the previously
     // provided global CONCENTRATION_BOUNDS. If a value was provided, we can
-    // update the global integer of concentration_bounds.
-    if (argc != 2) {
-        concentration_bounds = CONCENTRATION_BOUNDS;
+    // update the global integer of concentration_bounds. - Sean H.
+    if (argc >= 2) {
+		num_threads = atoi(argv[1]);
+    } else if (argv >= 3) {
+        concentration_bounds = atoi(argv[2]);
     } else {
-        concentration_bounds = atoi(argv[1]);
-    }
+		num_threads = NUMT;
+		concentration_bounds = CONCENTRATION_BOUNDS;
+	}
+
+#ifdef _OPENMP
+		fprintf( stderr, "OpenMP is supported -- version = %d\n", _OPENMP );
+#else
+        fprintf( stderr, "No OpenMP support!\n" );
+        return 1;
+#endif
+
+	omp_set_num_threads( num_threads );    // set the number of threads to use in parallelizing the for-loop:`
 
     // initialize result arrays ----------------------------------------
 
     // All of the values of the results arrays should be set to -1.f, except for the very
     // first elements, which must be set to 0.f
+	// Equivalent to:
+	// double* interstitials = (double*)malloc(concentration_bounds * sizeof(double));
     double* interstitials = (double*)calloc(concentration_bounds, sizeof(double));
+	// double* vacancies = (double*)malloc(concentration_bounds * sizeof(double));
     double* vacancies = (double*)calloc(concentration_bounds, sizeof(double));
 
     // malloc() and calloc() return a value of NULL if the memory allocation failed. We need to
     // test for that.
     if (interstitials == NULL) {
         fprintf(stderr, "An error occurred when allocating memory for the interstitial array.\n");
-        return 1;
+        return 2;
     } else if (vacancies == NULL) {
         fprintf(stderr, "An error occurred when allocating memory for the vacancies array.\n");
-        return 2;
+        return 3;
     } else
         fprintf(stdout, "$d Bytes of memory was successfully allocated for both the interstitial and vacancy arrays.", concentration_bounds * sizeof(double));
 
 
-    // Set the first characters of the interstitials and vacancies arrays to
-    // be 0.f.
+    // If the arrays were successfully allocated memory, let the first characters of the interstitials
+	// and vacancies arrays be set to 0.f.
     interstitials[0] = 0.f;
     vacancies[0] = 0.f;
 
-    // Using memset to initialize the rest or each array to be full
-    // of -1.f.
+    // Using memset to initialize the remaining values of each array to be -1.f
     // IMPORTANT: We need to offset the arrays by the size of the double datatype to
     // ensure we only overwrite the indices from 1 to concentration_bounds - 1.
     // As such, we are only setting (concentration_bounds - 1) * sizeof(double)
@@ -175,6 +202,9 @@ int main(int argc, char* argv[])
     // -----------------------------------------------------------------
 
     // calculate interstitial / vacancy concentrations -----------------
+    // Candidate for parallelization on the CPU through functional decomposition.
+	// Need to research further to see if we can parallize these functions on a GPU. - Sean H.
+	#pragma omp parallel for default(none) shared(concentration_bounds, interstitials, vacancies)
     for (int n = 1; n < concentration_bounds; ++n)
     {
         interstitials[n] = i_clusters(n, interstitials, OSIRIS, SA304);
@@ -183,14 +213,21 @@ int main(int argc, char* argv[])
     // -----------------------------------------------------------------
 
     // print results ---------------------------------------------------
+#ifdef	CSV
+    for (int n = 1; n < concentration_bounds; ++n)
+    {
+        fprintf(stdout, "%d, %8.1f, %8.1f", n, interstitials[n], vacancies[n]);
+    }
+#else
     fprintf(stdout, "Cluster Size\t-\tInterstitials\t-\tVacancies\n\n");
     for (int n = 1; n < concentration_bounds; ++n)
     {
         fprintf(stdout, "%d\t\t\t%8.1f\t\t%8.1f\n\n", n, interstitials[n], vacancies[n]);
     }
+#endif
     // -----------------------------------------------------------------
 
-    // Free up the dynamically allocated arrays.
+    // Free up the dynamically allocated arrays. THIS IS ESSENTIAL TO AVOID MEMORY LEAKS! - Sean H.
     free(interstitials);
     free(vacancies);
 
